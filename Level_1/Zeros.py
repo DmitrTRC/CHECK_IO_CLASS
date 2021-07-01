f1789fceb96d9841d2f6e433db2558772c2771f0
Dmitry Morozov <9984398@gmail.com>
2021-07-01 18:30:28 +0300

    Next Part Added

---
+++ Level_1/Zeros.py
@@ -0,0 +1,19 @@
+def end_zeros(num : int) -> int:
+    for index, char in enumerate(reversed(str(num))):
+        if  char == '0' : continue
+        else: return index
+    return str(num).__len__()
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(end_zeros(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert end_zeros(0) == 1
+    assert end_zeros(1) == 0
+    assert end_zeros(10) == 1
+    assert end_zeros(101) == 0
+    assert end_zeros(245) == 0
+    assert end_zeros(100100) == 2
+    print("Coding complete? Click 'Check' to earn cool rewards!")
