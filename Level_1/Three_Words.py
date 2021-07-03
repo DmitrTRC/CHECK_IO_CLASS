a4d3e0f4c00b0d2302c0283dd361810a53be0eab
Dmitry Morozov <9984398@gmail.com>
2021-07-03 09:50:56 +0300

    Failed. Need to check result of assert

--- Level_1/Three_Words.py
+++ Level_1/Three_Words.py
@@ -1,25 +1,39 @@
-def check_counter(word):
+def check_counter(word) -> bool:
     counter = 0
-    def add_and_check(): # Better to pass argument word
+
+    def count_word():  # Better to pass argument word
         nonlocal counter
-        counter += 1 if word.isalpha() else 0
+        print(f'Counter before : {counter}')
+        if word.isalpha():
+            counter += 1
+        elif counter == 3:
+            counter = -1
+        else:
+            counter = 0
+        print(f'Counter after : {counter}')
+
         return counter
-    return True if counter == 3 else False
+    return count_word
+
 
 def checkio(words: str) -> bool:
     for word in words.split():
-        if not check_counter(word): continue 
-        else: return True
-    return False    
-    
+        if check_counter(word) == -1:
+            return True
+        else:
+            continue
 
-#These "asserts" using only for self-checking and not necessary for auto-testing
+    return False
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     print('Example:')
     print(checkio("Hello World hello"))
-    
+
     assert checkio("Hello World hello") == True, "Hello"
-    assert checkio("He is 123 man") == False, "123 man"
+    assert checkio("He is 123 man") == False, 'Result is : ' + \
+        str(checkio("He is 123 man"))
     assert checkio("1 2 3 4") == False, "Digits"
     assert checkio("bla bla bla bla") == True, "Bla Bla"
     assert checkio("Hi") == False, "Hi"
