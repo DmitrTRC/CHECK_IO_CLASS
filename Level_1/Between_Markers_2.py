aedd144e08ef884ef1c0147ab30fa6eff188dcf1
dmitrytrc <9984398@gmail.com>
2021-07-04 20:09:55 +0300

    Marker Class Started

--- Level_1/Between_Markers_2.py
+++ Level_1/Between_Markers_2.py
@@ -11,12 +11,26 @@ Input: Three arguments. All of them are strings. The second and third arguments 
 Output: A string.
 '''
 
+
+class Marker:
+    def __init__(self, symbol: str, position: int = 0):
+        self.position = position
+        self.symbol = symbol
+
+    def seek(text: str):
+        pass
+
+
+class Boundary:
+    def __init__(self, text: str, begin: str, end: str):
+
+
 def between_markers(text: str, begin: str, end: str) -> str:
     """
         returns substring between two given markers
     """
-    
-    return  text[text.index(begin)+1:text.index(end)]
+
+    return text[text.index(begin)+1:text.index(end)]
 
 
 if __name__ == '__main__':
@@ -29,6 +43,7 @@ if __name__ == '__main__':
                            "<title>", "</title>") == "My new site", "HTML"
     assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
     assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
-    assert between_markers('No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
+    assert between_markers(
+        'No hi', '[b]', '[/b]') == 'No hi', 'No markers at all'
     assert between_markers('No <hi>', '>', '<') == '', 'Wrong direction'
     print('Wow, you are doing pretty good. Time to check it!')
