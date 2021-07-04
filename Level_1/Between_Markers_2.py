9d878e8d0b1cd8113cfe131b28ca3895c2aa3c57
Dmitry Morozov <9984398@gmail.com>
2021-07-05 01:15:14 +0300

    Completed. TESTED. Not refactored.

--- Level_1/Between_Markers_2.py
+++ Level_1/Between_Markers_2.py
@@ -17,11 +17,19 @@ class Marker:
         self._position = position
         self.symbol = symbol
 
+    def position(self): return self._position
+
     def seek(self, source: str):
-        self.position = source.find(self.symbol)
+        self._position = source.find(self.symbol)
 
     def set(self, value):
-        self.position = value
+        self._position = value
+
+    def __eq__(self, other):
+        return self._position == other._position
+
+    def __gt__(self, other):
+        return self._position > other._position
 
 
 class Boundary:
@@ -30,29 +38,37 @@ class Boundary:
         assert begin != end, f'Begin must be different from end'
         self._source = text
         self._end_position = text.__len__()
-        self.initial_marker = Marker(begin).seek(self._source)
-        self.final_marker = Marker(end).seek(self._source)
+        self.initial_marker = Marker(begin)
+        self.initial_marker.seek(self._source)
+        if self.initial_marker.position() != -1 :
+            self.initial_marker.set(self.initial_marker.position() + begin.__len__())
+
+        self.final_marker = Marker(end)
+        self.final_marker.seek(self._source)
 
     def _logic_check(self):
-        if not self.initial_marker:
+        if self.initial_marker.position() == -1:
             self.initial_marker.set(0)
 
-        if not self.final_marker:
+        if self.final_marker.position() == -1:
             self.final_marker.set(self._end_position)
-        
+
         if self.initial_marker > self.final_marker:
             self.initial_marker.set(0)
             self.final_marker.set(0)
 
+    def run(self) -> str:
+        self._logic_check()
+        return self._source[self.initial_marker.position():self.final_marker.position()]
 
-        
 
 def between_markers(text: str, begin: str, end: str) -> str:
     """
         returns substring between two given markers
     """
 
-    return text[text.index(begin)+1:text.index(end)]
+    boundary = Boundary(text, begin, end)
+    return boundary.run()
 
 
 if __name__ == '__main__':
@@ -60,9 +76,11 @@ if __name__ == '__main__':
     print(between_markers('What is >apple<', '>', '<'))
 
     # These "asserts" are used for self-checking and not for testing
-    assert between_markers('What is >apple<', '>', '<') == "apple", "One sym"
+    assert between_markers(
+        'What is >apple<', '>', '<') == "apple", f" Got : { between_markers('What is >apple<', '>', '<')}"
     assert between_markers("<head><title>My new site</title></head>",
-                           "<title>", "</title>") == "My new site", "HTML"
+                           "<title>", "</title>") == "My new site",  between_markers("<head><title>My new site</title></head>",
+                                                                                     "<title>", "</title>")
     assert between_markers('No[/b] hi', '[b]', '[/b]') == 'No', 'No opened'
     assert between_markers('No [b]hi', '[b]', '[/b]') == 'hi', 'No close'
     assert between_markers(
