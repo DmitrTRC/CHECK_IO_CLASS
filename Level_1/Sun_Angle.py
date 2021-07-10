f30b3034120ab0e0ab89fdac3c0f5347558a3d1f
Dmitry <9984398@gmail.com>
2021-07-10 16:29:44 +0200

    Completed. Tested

--- Level_1/Sun_Angle.py
+++ Level_1/Sun_Angle.py
@@ -1,12 +1,23 @@
 from typing import Union
+from datetime import timedelta, datetime
 
 
-def sun_angle(time: str) -> Union[int, str]:
-    # replace this for solution
-    return time
+def sun_angle(time_srs: str) -> Union[int, str]:
+    SUN_UP_TIME = datetime.strptime("06:00", "%H:%M")
+    SUN_DOWN_TIME = datetime.strptime("18:00", "%H:%M")
+
+    cur_time = datetime.strptime(time_srs, "%H:%M")
+
+    if cur_time < SUN_UP_TIME or cur_time > SUN_DOWN_TIME:
+        return "I don't see the sun!"
+    resolution = 180 / (
+        (SUN_DOWN_TIME - SUN_UP_TIME).seconds / 60
+    )  # Resolution : Degree per minutes
+
+    return round(((cur_time - SUN_UP_TIME).seconds / 60) * resolution, 2)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     print("Example:")
     print(sun_angle("07:00"))
 
