a3aab59b6c46124f1e3c765026ae3a6834bd62c9
Dmitry <9984398@gmail.com>
2021-07-12 22:31:31 +0200

    Refactored. Tested

--- Level_1/date_time_converter.py
+++ Level_1/date_time_converter.py
@@ -1,19 +1,21 @@
-from  datetime import datetime
+from datetime import datetime
 
 
 def date_time(time: str) -> str:
-    dt_converted  = datetime.strptime(time, "%d.%m.%Y %H:%M")
-    return dt_converted.strftime("%-d %B %Y year %-H hours %-M minutes")
-
+    dt_converted = datetime.strptime(time, "%d.%m.%Y %H:%M")
+    return dt_converted.strftime(
+        f"%-d %B %Y year %-H hour{'s' if dt_converted.hour != 1 else ''} %-M minute{'s' if dt_converted.minute != 1 else ''}" 
+    )
 
 if __name__ == "__main__":
     print("Example:")
-    print(date_time("01.01.2000 00:00"))
+    print(date_time("01.01.2000 01:01"))
 
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert (
         date_time("01.01.2000 00:00") == "1 January 2000 year 0 hours 0 minutes"
     ), "Millenium"
+
     assert (
         date_time("09.05.1945 06:30") == "9 May 1945 year 6 hours 30 minutes"
     ), "Victory"
